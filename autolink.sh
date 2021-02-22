1be99943e03be58d41d224edea1d28a2430e460e
fabripetrelli <petrellifabriziopaolo@gmail.com>
2021-02-22 19:06:45 +0100

    Fix

--- autolink.sh
+++ autolink.sh
@@ -4,10 +4,10 @@ git clone https://github.com/t413/zsh-background-notify.git .zsh-plugins
 
 ln -sv .aliases ~
 ln -sv .hyper.js ~
-ln -sv .hyper_plugins ~
-ln -sv .oh-my-zsh ~
-ln -sv .themes ~
-ln -sv .zsh-plugins ~
+ln -sv .hyper_plugins/ ~
+ln -sv .oh-my-zsh/ ~
+ln -sv .themes/ ~
+ln -sv .zsh-plugins/ ~
 ln -sv .zshrc ~
 
 source ~/.zshrc
