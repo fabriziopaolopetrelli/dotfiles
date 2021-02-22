cc6c8e248c3937fdbe144e8e4e24e8943582b5df
fabripetrelli <petrellifabriziopaolo@gmail.com>
2021-02-22 19:22:17 +0100

    Autolink script now works properly

--- autolink.sh
+++ autolink.sh
@@ -1,13 +1,13 @@
-#!/bin/sh
+
 #linking all the folders in right position
 git clone https://github.com/t413/zsh-background-notify.git .zsh-plugins
 
-ln -sv .aliases ~
-ln -sv .hyper.js ~
-ln -sv .hyper_plugins/ ~
-ln -sv .oh-my-zsh/ ~
-ln -sv .themes/ ~
-ln -sv .zsh-plugins/ ~
-ln -sv .zshrc ~
+ln -sv Progetti/dotfiles/.aliases ~
+ln -sv Progetti/dotfiles/.hyper.js ~
+ln -sv Progetti/dotfiles/.hyper_plugins ~
+ln -sv Progetti/dotfiles/.oh-my-zsh ~
+ln -sv Progetti/dotfiles/.themes ~
+ln -sv Progetti/dotfiles/.zsh-plugins ~
+ln -sv Progetti/dotfiles/.zshrc ~
 
 source ~/.zshrc
